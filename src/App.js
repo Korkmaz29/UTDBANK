eead27e900b520d3ce4729128f836474b4b01274
LEVENT <akmirza@gmail.com>
2022-01-11 15:13:26 +0100

    b1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
-function App() {
-  return <div className="App">Merhaba Dünya</div>;
+import React from 'react'
+import BannerStart from './components/common/BannerStart'
+import Menu from './components/common/Menu'
+import MenuStart from './components/common/MenuStart'
+import Top  from './components/common/Top' 
+
+
+const App = () => {
+  return (
+    <div>
+     <Top/>
+     <Menu/>
+     <MenuStart/>
+     <BannerStart/>
+     
+
+
+
+      
+          </div>
+  )
 }
 
-export default App;
+export default App
