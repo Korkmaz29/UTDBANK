673847f690a12d1b8077a806ba29628467eecf63
LEVENT <akmirza@gmail.com>
2022-01-23 22:35:18 +0100

    Levent

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from 'react'
-import BannerStart from './components/common/BannerStart'
-import Menu from './components/common/Menu'
-import MenuStart from './components/common/MenuStart'
-import Top  from './components/common/Top' 
-
+import Banner from './components/common/Banner'
+import BannerContact from './components/common/BannerContact'
+import MenuStart_Log from './components/common/MenuStart_Log'
+import FeaturTitle from './components/common/FeaturTitle'
+import Hom from './components/common/Hom'
+import Cont from './components/common/Cont'
+import Service from './components/common/Service'
+import Pricing from './components/common/Pricing'
+import Facility from './components/common/Facility'
+import HomContact from './components/common/HomContact'
+import Tertim from './components/common/Tertim'
+import Flooter from './components/common/Flooter'
+import TopBar from './components/common/TopBar'
 
 const App = () => {
   return (
-    <div>
-     <Top/>
-     <Menu/>
-     <MenuStart/>
-     <BannerStart/>
+   
+   <div>
+     <Home/>
      
-
-
-
-      
-          </div>
+   </div>
   )
 }
 
